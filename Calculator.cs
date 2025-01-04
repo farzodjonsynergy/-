23272e500be3625d3834905c72f3658a891f760f
farzodjonsynergy <farzod41@gmail.com>
2025-01-04 16:12:09 +0500

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число:");
+        double operand1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        double operand2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите знак операции (+, -, *, /):");
+        string sign = Console.ReadLine();
+
+        if (sign == "+")
+        {
+            Console.WriteLine($"Результат: {operand1 + operand2}");
+        }
+        else if (sign == "-")
+        {
+            Console.WriteLine($"Результат: {operand1 - operand2}");
+        }
+        else if (sign == "*")
+        {
+            Console.WriteLine($"Результат: {operand1 * operand2}");
+        }
+        else if (sign == "/")
+        {
+            if (operand2 != 0)
+            {
+                Console.WriteLine($"Результат: {operand1 / operand2}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: деление на ноль невозможно.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: неверный знак операции.");
+        }
+
+        Console.WriteLine("Нажмите любую клавишу для завершения...");
+        Console.ReadKey();
+    }
+}
---
+++ Задача 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значения переменной А:");
+        int A  = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите число для переменной B:");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        if (A != B)
+        {
+            int max = Math.Max(A, B);
+            A = max;
+            B = max;
+        }
+        else
+        {
+            A = 0;
+            B = 0;
+
+        }
+        Console.WriteLine($"Новое значение А: {A}");
+        Console.WriteLine($"Новое значение B: {B}");
+    }
+}
+
